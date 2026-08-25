c8356a13104263bae1b2615ffb9194d7b747db14
chandegaramayank-cpu <chandegaramayank@gmail.com>
2026-08-25 12:21:40 +0530

    Project

---
+++ Portal/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import login from './pages/Login.jsx'
+const App = () => {
+  return (<>
+    <login/>
+    <div>Student Portal</div>
+  </>
+  )
+}
+
+export default App
